9480439c2771259fe8e5664d072024c496e74544
Dylan <dylanrose60@gmail.com>
2017-04-11 01:17:12 -0400

    new layout for scrolling

--- main.js
+++ main.js
@@ -1,3 +1,57 @@
 $(document).ready( function() {
-  $('.rest').css( 'margin-top', $('.nav').height() );
+  $('.home-content').css( 'padding-top', $('.nav').height() / 2 ); // Need to add the 2 em of the navs margin to this number
+
+$(function() {
+  $.scrollify({
+    section: '.section',
+    scrollbars: false,
+    scrollSpeed: 1500,
+    before: function( toSection ) {
+
+      // currentSection = 1 means heading to the below section
+      // currentSection of 0 means heading to the above home section
+
+      if( toSection == 1 ) {
+
+        $('.home-content').fadeOut( 70 );
+
+      } else if( toSection == 0 ) {
+
+        setTimeout( function() {
+          $('.home-content').fadeIn();
+        }, 300 );
+
+      }
+
+    },
+    after: function( currentSection ) {
+
+      // currentSection = 1 means heading to the below section
+      // currentSection of 0 means heading to the above home section
+
+      /**
+      if( currentSection == 0 ) {
+        $('.home-content').fadeIn();
+        $('#home').css( 'color', '#4484C8' );
+        $('#pricing').css( 'color', '#ecf0f1' );
+      } else {
+        $('#pricing').css( 'color', '#4484C8' );
+        $('#home').css( 'color', '#ecf0f1' );
+      }
+      */
+
+    }
+  });
+});
+
+  /* Fade out home container when scrolled up */
+
 } );
+
+/**
+
+$(window).scroll( function(event) {
+  var scrollAmount = $(window).scrollTop();
+});
+
+*/
