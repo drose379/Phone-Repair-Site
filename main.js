b7c9dda428aa6c9bc77a593f3e39124064023463
Dylan <dylanrose60@gmail.com>
2017-04-12 23:42:13 -0400

    device type same width

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ $(document).ready( function() {
   $('.home-content').css( 'padding-top', $('.nav').height() / 2 ); // Need to add the 2 em of the navs margin to this number
   $('.pricing-ui').css( 'padding-top', $('.nav').height() / 2 );
 
+/*
   console.log( $('#ipad').outerWidth() );
   $('#iphone').outerWidth( $('#ipad').outerWidth() );
+*/
 
 $(function() {
   $.scrollify({
